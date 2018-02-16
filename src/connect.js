e8d00a4091056e099fbd4eed5db010157829bc28
Peter Shev <piotr.sevcenco@endava.com>
2018-02-16 22:18:43 +0200

    Fix indents

--- src/connect.js
+++ src/connect.js
@@ -2,16 +2,16 @@ import {h} from 'preact'
 import mapValues from 'map-values'
 
 export function connect(rules) {
-	const finalRules = props => {
-		const rulesObj = typeof rules === 'function' ? rules(props) : rules
+  const finalRules = props => {
+    const rulesObj = typeof rules === 'function' ? rules(props) : rules
     // wrap rule objects in a function call because that's how fela's "renderRule" function expects them
     return mapValues(rulesObj, rule => (() => rule))
-	}
+  }
 
-	return WrappedComponent => (props, context) => {
-		const propsWithTheme = Object.assign({}, props, {theme: context.theme || {}})
+  return WrappedComponent => (props, context) => {
+    const propsWithTheme = Object.assign({}, props, {theme: context.theme || {}})
 
-		return h(WrappedComponent, Object.assign({}, props, {
+    return h(WrappedComponent, Object.assign({}, props, {
       styles: mapValues(finalRules(propsWithTheme), rule => context.renderer.renderRule(rule))
     }))
   }
